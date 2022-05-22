e65ba7e9d712082a98176159d49cc76b86c8bac0
Giuseppe Migliaccio <giuseppe.migliaccio93@gmail.com>
2022-05-22 22:01:35 +0200

    refactoring

--- lib/widgets/initiative/footer/initiative_footer_tag.dart
+++ lib/widgets/initiative/footer/initiative_footer_tag.dart
@@ -11,7 +11,7 @@ class InitiativeFooterTag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
       child: Text(
         tag,
         style: TextStyle(
