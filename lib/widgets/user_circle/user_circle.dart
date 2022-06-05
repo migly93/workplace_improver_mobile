8d4dfcd58194ab72298e845e2d0a0a08141910fc
Giuseppe Migliaccio <giuseppe.migliaccio93@gmail.com>
2022-06-05 13:14:39 +0200

    created user profile bottom sheet

--- lib/widgets/user_circle/user_circle.dart
+++ lib/widgets/user_circle/user_circle.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import '../user_profile/user_profile.dart';
+import '../../models/initiative.dart';
 import '../../models/user.dart';
 import '../../utils/constants.dart';
 import '../../utils/utilities.dart';
 import '../clickable_widget/clickable_widget.dart';
 
 class UserCircle extends StatelessWidget {
+  final User loggedUser;
   final User user;
   final double radius;
   final bool clickable;
+  final Initiative? initiative;
 
   const UserCircle({
     Key? key,
+    required this.loggedUser,
     required this.user,
     required this.radius,
-    this.clickable = true,
+    required this.clickable,
+    this.initiative,
   }) : super(key: key);
+
+  void showUserProfile(BuildContext context) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (_) {
+          return DraggableScrollableSheet(
+            initialChildSize: 0.95,
+            builder: (_, controller) => UserProfile(
+              loggedUser: loggedUser,
+              profileToShow: user,
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +56,9 @@ class UserCircle extends StatelessWidget {
               )
             : null,
       ),
-      onPressed: clickable ? () {} : null,
+      onPressed: clickable && loggedUser.id != user.id
+          ? () => showUserProfile(context)
+          : null,
     );
   }
 }
